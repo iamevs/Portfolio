5542251116450f56a3054c1a71af4175ee967037
iamevs <iamevs143@gmail.com>
2023-03-07 18:59:54 +0530

    3D objects added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,30 @@
+import { motion } from 'framer-motion'
+
+import { styles } from "../styles";
+
 const Hero = () => {
     return (
-        <div className="relative z-0 bg-primary">
-            <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-                <div className="flex flex-col justify-center items-center h-screen">
-                    <h1 className="text-6xl text-white font-bold">Hello, I'm <span className="text-secondary">EVS</span></h1>
-                    <h2 className="text-2xl text-white font-bold">I'm a FullStack Developer</h2>
-                </div>
+        <section className={`relative w-full h-screen mx-auto`}>
+          <div
+            className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+          >
+            <div className='flex flex-col justify-center items-center mt-5'>
+              <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+              <div className='w-1 sm:h-80 h-40 violet-gradient' />
             </div>
-        </div>
-    )
+    
+            <div>
+              <h1 className={`${styles.heroHeadText} text-white`}>
+                Hi, I'm <span className='text-[#915EFF]'>EVS</span>
+              </h1>
+              <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+                I develop 3D visuals, user <br className='sm:block hidden' />
+                interfaces and web applications
+              </p>
+            </div>
+          </div>
+        </section>
+      );
 }
 
 export default Hero
